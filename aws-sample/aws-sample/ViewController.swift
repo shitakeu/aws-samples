60e63e0b019aa6ab298168fb1807b22567a84b1d
shitakeu <sin5.take@gmail.com>
2019-04-28 11:16:49 +0900

    init commit

---
+++ aws-sample/aws-sample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  aws-sample
+//
+//  Created by 竹内 真吾 on 2019/04/28.
+//  Copyright © 2019 twentiethfloor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ aws-sample/aws-sampleTests/aws_sampleTests.swift
@@ -0,0 +1,34 @@
+//
+//  aws_sampleTests.swift
+//  aws-sampleTests
+//
+//  Created by 竹内 真吾 on 2019/04/28.
+//  Copyright © 2019 twentiethfloor. All rights reserved.
+//
+
+import XCTest
+@testable import aws_sample
+
+class aws_sampleTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
